39af0391771eb07a9544de0855b073102f9d8c2b
DevAch33 <achaibane.ayoub@gmail.com>
2023-07-26 16:31:53 -0700

    done

--- moreFunctions1.c
+++ moreFunctions1.c
@@ -1,78 +1,38 @@
 #include "shell.h"
+
 /**
- * interactive - Returns true if the shell is in interactive mode.
+ * interactive - Returns true if shell is in interactive mode.
  * @info: struct address.
- *
- * Return: 1 if in interactive mode, 0 otherwise.
+ * Return: 1 if interactive mode, 0 otherwise.
  */
 int interactive(info_t *info)
 {
 	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
-/** is_delim - Checks if a character is a delimiter.
- * @c: The character to check.
+/**
+ * is_delim - Checks if a character is a delimiter.
+ * @c: The char to check.
  * @delim: The delimiter string.
  * Return: 1 if true, 0 if false.
  */
 int is_delim(char c, char *delim)
 {
 	while (*delim)
-	{
-		if (*delim == c)
+		if (*delim++ == c)
 			return (1);
-		delim++;
-	}
 	return (0);
 }
 
 /**
- * _isalpha - Checks for an alphabetic character.
+ * _isalpha - Checks for alphabetic character.
  * @c: The character to input.
- *
  * Return: 1 if c is alphabetic, 0 otherwise.
  */
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-}
-
-/**
- * _atoi - Converts a string to an integer.
- * @s: The string to be converted.
- *
- * Return: 0 if no numbers in the string, the converted number otherwise.
- */
-int _atoi(char *s)
-{
-	int i = 0;
-	int sign = 1;
-	unsigned int result = 0;
-	int flag = 0;
-	int output;
-
-	while (s[i] != '\0' && flag != 2)
-	{
-		if (s[i] == '-')
-			sign *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			flag = 1;
-			result = result * 10 + (s[i] - '0');
-		}
-		else if (flag == 1)
-		{
-			flag = 2;
-		}
-		i++;
-	}
-
-	if (sign == -1)
-		output = -result;
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
 	else
-		output = result;
-
-	return (output);
+		return (0);
 }
-
