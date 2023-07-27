3eb6bb0d49316842e5fb337dd6d2cedbc924ab0c
DevAch33 <124171054+DevAch33@users.noreply.github.com>
2023-07-27 22:27:39 +0100

    Update getLine.c

--- getLine.c
+++ getLine.c
@@ -44,51 +44,6 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 }
 
 /**
- * get_input - Gets a line minus the newline.
- * @info: Pointer to the parameter struct.
- *
- * Return: Bytes read.
- */
-ssize_t get_input(info_t *info)
-{
-	static char *buf;
-	static size_t i, j, len;
-	size_t r = 0;
-	char **buf_p = &(info->ag), *p;
-
-	_putchar(BUF_FLUSH);
-	r = input_buf(info, &buf, &len);
-	if (r == -1)
-		return (-1);
-	if (len)
-	{
-		j = i;
-		p = buf + i;
-
-		check_chain(info, buf, &j, i, len);
-		while (j < len)
-		{
-			if (is_chain(info, buf, &j))
-				break;
-			j++;
-		}
-
-		i = j + 1;
-		if (i >= len)
-		{
-			i = len = 0;
-			info->cmd_buf_type = CMD_NORM;
-		}
-
-		*buf_p = p;
-		return (_strlen(p));
-	}
-
-	*buf_p = buf;
-	return (r);
-}
-
-/**
  * read_buf - Reads a buffer.
  * @info: Pointer to the parameter struct.
  * @buf: Buffer.
