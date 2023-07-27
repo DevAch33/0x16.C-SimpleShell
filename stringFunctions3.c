8426d078ef2bc52ca9365aa359717039d634ca86
DevAch33 <124171054+DevAch33@users.noreply.github.com>
2023-07-27 16:31:32 +0100

    Update stringFunctions3.c

--- stringFunctions3.c
+++ stringFunctions3.c
@@ -46,4 +46,18 @@ char **strtow(char *str, char *d)
 	s[j] = NULL;
 	return (s);
 }
+/**
+ * starts_with - checks if needle starts with haystack
+ * @haystack: string to search
+ * @needle: the substring to find
+ *
+ * Return: address of next char of haystack or NULL
+ */
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
 
